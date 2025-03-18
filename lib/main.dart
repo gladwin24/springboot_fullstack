45fc5cacc1ed4771dc9280e0f22fef2a6069c932
Aalap Manoj <aalapmanoj@gmail.com>
2025-03-18 11:55:46 +0530

    feat: working chekpoint

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'services/auth_service.dart';
 import 'login_page.dart';
 import 'register_page.dart';
 import 'home_page.dart';
 import 'theme/app_theme.dart';
-import 'firebase_options.dart';
+// import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
   runApp(
     MultiProvider(
       providers: [
